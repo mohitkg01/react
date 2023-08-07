5f5e2e00a3c98227da8812bb50448ee73b3e5f73
Mohit <mohitkg2601@gmail.com>
2023-08-07 16:40:18 +0530

    expenseitems basic

---
+++ firstapp/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItems from "./components/ExpenseItems";
+function App() {
+  return (
+  <>
+  <ExpenseItems/>
+  </>
+  
+    
+  );
+}
+
+export default App;
---
+++ firstapp/src/components/ExpenseItems.js
@@ -0,0 +1,12 @@
+function ExpenseItems(){
+    return (
+    <>
+    <h2>Expense Items </h2>
+    <h2><div>Food Rs 10 </div>
+    <div>Petrol Rs 100</div>
+    <div>Movies Rs 200</div></h2>
+    </>
+    
+    )
+}
+export  default ExpenseItems;
