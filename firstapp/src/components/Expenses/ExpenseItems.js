b8b6ed5d421b37f9c3c58e686fa679cf62210a02
Mohit <mohitkg2601@gmail.com>
2023-08-11 00:18:14 +0530

    doing

--- firstapp/src/components/Expenses/ExpenseItems.js
+++ firstapp/src/components/Expenses/ExpenseItems.js
@@ -4,12 +4,20 @@ import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css'
 
 const ExpenseItems=(props)=>{
+    const clickHandler=()=>{
+        console.log('clickedd!!!');
+    }
+    const DeleteHandler=()=>{
+        console.log("Deleted")
+    }
     
     return (
    <Card className="expense-item">
     <ExpenseDate date={props.date}/>
     <ExpenseDetails amount={props.amount} 
     loc={props.loc} title={props.title}/>
+    <button onClick={clickHandler}>Change Title</button>
+    <button onClick={DeleteHandler}>Delete</button>
     </Card>
     );
 }
