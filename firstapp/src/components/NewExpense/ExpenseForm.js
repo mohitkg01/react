7180fe12c4590d36d9a16f927f403530ffc722ea
Mohit <mohitkg2601@gmail.com>
2023-08-11 23:34:43 +0530

    adding three states updateDate,updatetitle,updateAmount

--- firstapp/src/components/NewExpense/ExpenseForm.js
+++ firstapp/src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const ChangeHandler=(event)=>{
-    console.log(event.target.value);
+  const [enteredtitle,setenteredTitle]=useState('');
+  const [amountEntered,setAmount]=useState('');
+  const [enterdDate,setDate]=useState('');
+
+  const titleHandler=(event)=>{
+    setenteredTitle(event.target.value);
+    
+  };
+  const amountHandler=(event)=>{
+    setAmount(event.target.value);
+  }
+  const dateHandler=(event)=>{
+    setDate(event.target.value);
   }
   return (
         <form>
           <div className="new-expense-controls">
             <div className="new-expense-control">
             <label>Expense Title</label>
-            <input type="text" onChange={ChangeHandler}/>
+            <input type="text" onChange={titleHandler}/>
             <label>Expense Amount</label>
-            <input type="number"  onChange={ChangeHandler} required/>
+            <input type="number"  onChange={amountHandler} required/>
             <label>Expense Date</label>
-            <input type="date"  required/>
+            <input type="date"  onChange={dateHandler} required/>
             </div>  
           </div>
           <div className="new-expense-action">
