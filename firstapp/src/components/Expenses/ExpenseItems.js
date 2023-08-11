aa831c5ab62b520fd497d8ca0752da7b06fd584c
Mohit <mohitkg2601@gmail.com>
2023-08-11 10:02:28 +0530

    adding expense changer

--- firstapp/src/components/Expenses/ExpenseItems.js
+++ firstapp/src/components/Expenses/ExpenseItems.js
@@ -1,23 +1,30 @@
+import React,{useState} from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css'
 
 const ExpenseItems=(props)=>{
+   const [amount,setTitle]= useState(props.amount);
+
     const clickHandler=()=>{
-        console.log('clickedd!!!');
+        
+        console.log("clicked");
     }
     const DeleteHandler=()=>{
         console.log("Deleted")
     }
-    
+    const expenseUpdate=()=>{
+        setTitle("$100");
+    }
     return (
    <Card className="expense-item">
     <ExpenseDate date={props.date}/>
-    <ExpenseDetails amount={props.amount} 
+    <ExpenseDetails amount={amount} 
     loc={props.loc} title={props.title}/>
     <button onClick={clickHandler}>Change Title</button>
     <button onClick={DeleteHandler}>Delete</button>
+    <button onClick={expenseUpdate}> Expense</button>
     </Card>
     );
 }
