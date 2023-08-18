cf6599f78f77812c1e2a148cc6d98f256cc741f0
Mohit <mohitkg2601@gmail.com>
2023-08-19 02:30:22 +0530

    ashowing in UI no of Expenses

--- firstapp/src/components/Expenses/Expensese.js
+++ firstapp/src/components/Expenses/Expensese.js
@@ -14,20 +14,37 @@ const Expenses=(props)=>{
     });
 
     const filteredExpenses=props.expense.filter(exp=>exp.date.getFullYear().toString()===filteredYear);
+  
+    let expenseContent =<p> No expense found</p>;
+    let lengthExpense=<p>{filteredExpenses.length} Expense is there</p>
+    if(filteredExpenses.length>0){
+        expenseContent=filteredExpenses.map((items)=> (
+            <ExpenseItem 
+                key={items.id}
+              title={items.expen} 
+              amount={items.amount} 
+              date={items.date}/>));
+    }
+   
     return(
         <div>
              <Card className="exp">
             <ExpensesFilter selected={filteredYear}
              onChangeFilter={filterChangedHandler}/>
-{
-    filteredExpenses.map((items)=>{return (
-<ExpenseItem 
-    key={items.id}
-  title={items.expen} 
-  amount={items.amount} 
-  date={items.date} 
-  loc={items.loc}/>)})}
-  
+              {lengthExpense}
+             {/* {filteredExpenses.length=== 0 ? (<p>No expense found.</p>):(filteredExpenses.map((items)=> (
+            <ExpenseItem 
+                key={items.id}
+              title={items.expen} 
+              amount={items.amount} 
+              date={items.date} 
+              loc={items.loc}/>)
+              )
+          )
+         } */ expenseContent}
+        
+       
+
   </Card>
 </div>
        
